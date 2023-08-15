02616808ba5dd0c1efaeee97f41867a36711592b
sakhyat <sakhyatkabir1@gmail.com>
2023-08-16 00:03:50 +0600

    updated

--- components/Checkout.jsx
+++ components/Checkout.jsx
@@ -44,7 +44,88 @@ const CheckoutPage = ({ course }) => {
 
   return (
     <div className="checkout flex justify-center">
-      
+      <form
+        onSubmit={handleCheckout}
+        className="w-full lg:w-[35rem] flex flex-col gap-5"
+      >
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="username">Name</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="jon"
+            readOnly
+            value={formData.name}
+            className="outline-none bg-inherit border py-3 px-4 rounded-md"
+          />
+        </div>
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            placeholder="jon@gmail.com"
+            readOnly
+            value={formData.email}
+            className="outline-none bg-inherit border py-3 px-4 rounded-md"
+          />
+        </div>
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="number">Phone Number</label>
+          <input
+            type="number"
+            id="number"
+            placeholder="017xxxxxxx"
+            value={formData.mobile}
+            onChange={(e) =>
+              setFormData({ ...formData, mobile: e.target.value })
+            }
+            className="outline-none bg-inherit border py-3 px-4 rounded-md"
+            required
+          />
+        </div>
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            placeholder="ABC NY 10000"
+            value={formData.address}
+            onChange={(e) =>
+              setFormData({ ...formData, address: e.target.value })
+            }
+            className="outline-none bg-inherit border py-3 px-4 rounded-md"
+          />
+        </div>
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="title">course title</label>
+          <input
+            type="text"
+            id="title"
+            placeholder="Course Title"
+            readOnly
+            value={formData.courseTitle}
+            className="outline-none bg-inherit border py-3 px-4 rounded-md"
+          />
+        </div>
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="price">Price</label>
+          <input
+            type="text"
+            id="price"
+            placeholder="price"
+            readOnly
+            value={currencyFormatter(formData.price)}
+            className="outline-none bg-inherit border py-3 px-4 rounded-md"
+          />
+        </div>
+        <button
+          role="link"
+          className="text-black bg-[#90EE90] hover:bg-white py-3"
+        >
+          Proceed to Checkout
+        </button>
+      </form>
     </div>
   );
 };
